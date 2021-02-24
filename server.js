366fac5617d05799b98191433559cff0ad1d75ab
Adly Magdy <1126032+adlymagdy@users.noreply.github.com>
2021-02-24 16:39:57 +0200

    - Initialize server with GET and POST Routes

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
+// Callback function to complete GET '/all'
+function sendData (req, res) {
+  res.send(projectData);
+}
+
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+
+/*Dependencies*/
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +22,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
-
+const port = 8000;
 
 // Setup Server
+const server = app.listen(port, listening);
+function listening() {
+  console.log("server running");
+  console.log(`running on localhost: ${port}`);
+}
+
+// POST Callback to debug
+function callback(req, res) {
+  res.send('Weather received')
+}
+
+// Initialize all route with a callback function
+app.get('/all', sendData);
+
+// POST Route
+app.post('/add', callback);

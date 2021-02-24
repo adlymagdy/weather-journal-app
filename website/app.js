2f2bbe9fe3d5531f0793c88a04e90234af59cdce
Adly Magdy <1126032+adlymagdy@users.noreply.github.com>
2021-02-24 16:44:10 +0200

    - Added API key and getWeather function to get weather data

--- website/app.js
+++ website/app.js
@@ -1,5 +1,29 @@
 /* Global Variables */
+// Base URL
+const baseURL = 'api.openweathermap.org/data/2.5/forecast?zip='
+// Personal API Key for OpenWeatherMap API
+const apiKEY = '&appid=5114736c30201eaa8f7077eacdeb3fe5'
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.getElementById('generate').addEventListener('click', generate)
+
+/* Function called by event listener */
+function generate (el) {
+  const zipCode = document.getElementById('zip').value;
+  getWeather(baseURL, zipCode, apiKEY)
+}
+
+/* Function to GET Web API Data*/
+const getWeather = async(baseURL, zipCode, key) => {
+  const res = await fetch (baseURL+zipCode+key)
+  try {
+    const weather = await res.json();
+    console.log(weather);
+    return weather;
+  } catch(err) {
+    console.log('error', error)
+  }
+}

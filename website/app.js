f4709b55c4e2de2ad6d6df83e410b08c522a4242
Adly Magdy <1126032+adlymagdy@users.noreply.github.com>
2021-02-24 18:35:08 +0200

    - getWeather function to get weather api data

--- website/app.js
+++ website/app.js
@@ -1,6 +1,6 @@
 /* Global Variables */
 // Base URL
-const baseURL = 'api.openweathermap.org/data/2.5/forecast?zip='
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip='
 // Personal API Key for OpenWeatherMap API
 const apiKEY = '&appid=5114736c30201eaa8f7077eacdeb3fe5'
 
@@ -18,12 +18,16 @@ function generate (el) {
 
 /* Function to GET Web API Data*/
 const getWeather = async(baseURL, zipCode, key) => {
-  const res = await fetch (baseURL+zipCode+key)
+  const res = await fetch (baseURL+zipCode+key+'&units=metric')
   try {
-    const weather = await res.json();
-    console.log(weather);
-    return weather;
+    const data = await res.json();
+    const forecast = data.weather[0].description;
+    const temperature = data.main.temp;
+    const name = data.name;
+    console.log(`Today is ${newDate} and the forecast for ${name}: ${forecast}`);
+    console.log(`It's currently ${temperature}°C `);
+    return data;
   } catch(err) {
-    console.log('error', error)
+    console.log('error', err)
   }
 }
